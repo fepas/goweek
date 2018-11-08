22c1e811fc3d680de899d0f8e2fd5095246e8b40
Felipe Campos <fepascampos@gmail.com>
2018-11-08 17:24:01 -0200

    adicionando mobile da aplicação
    Signed-off-by: Felipe Campos <fepascampos@gmail.com>

---
+++ mobile/src/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+import Routes from './routes';
+
+// import styles from './styles';
+
+export default class App extends Component {
+  render() {
+    return <Routes />;
+  }
+}
